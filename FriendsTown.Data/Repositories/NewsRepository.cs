156d43591a44540eb38ab7fa7c1d488cb784c587
Ernest <emujambere1@student.gsu.edu>
2022-10-23 23:23:32 -0400

    Add Client Side Validations!

---
+++ FriendsTown.Data/Repositories/NewsRepository.cs
@@ -0,0 +1,33 @@
+﻿using FriendsTown.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FriendsTown.Data.Repositories
+{
+    public class NewsRepository : INewsRepository
+    {
+        private readonly FriendsTownContext _contexto;
+
+        public NewsRepository(FriendsTownContext contexto) 
+        {
+            _contexto = contexto;
+        }
+
+        public void Add(News news) 
+        {
+            _contexto.Add(news);
+            _contexto.SaveChanges();
+        }
+
+        public News FindById(Guid id) 
+        {
+            return _contexto.News.Find(id);
+        }
+
+        public IEnumerable<News> GetAll() 
+        {
+            return _contexto.News.OrderBy(a => a.Date.Value);
+        }
+    }
+}
